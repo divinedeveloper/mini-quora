2c550645206c262172a364c7947b0a690b513fec
Swapnil Sawant <sawant.swapnil1@gmail.com>
2017-09-11 00:36:01 +0530

    completed search questions html page

--- core/static/js/controllers/QuestionsCtrl.js
+++ core/static/js/controllers/QuestionsCtrl.js
@@ -1,16 +1,23 @@
 myApp.controller('QuestionsCtrl', ['$scope', '$http', '$location', '$routeParams', 'toaster', function($scope, $http, $location, $routeParams, toaster) {
         $scope.successMessage;
         $scope.errorMessage;
-        $scope.getIecByCode = function(){
+        $scope.searchQuestions = function(){
             
             // $http({method: 'GET', url: 'iec/api/v1/retrieve/' {params:{"code": $scope.ieCode}} }).            
-            $http.get('iec/api/v1/retrieve/', {params:{"code": $scope.ieCode}}).
+            // $http.get('mini-quora/api/v1/questions/', {params: {"title": $scope.title}}, {headers: {'api-key': $scope.apiKey}}).
+
+            $http(
+                {
+                  method: 'GET', 
+                  url: 'mini-quora/api/v1/questions/', 
+                  headers: {'api-key': $scope.apiKey},
+                  params: {"title": $scope.title}
+
+                }).
                 success(function(data, status, headers, config) {
                        if(status == 200){
-//                          console.log(data)
-                        console.log(data.importer_exporter_code)
-                        $scope.responseIecData = data
-                           toaster.pop('success', "", "IEC data available");
+                        $scope.responseQuestions = data
+                           // toaster.pop('success', "", "");
                        }
 
                      }).
@@ -33,6 +40,9 @@ myApp.controller('QuestionsCtrl', ['$scope', '$http', '$location', '$routeParams
                     if(status == 404){
                         toaster.pop('error', "", data.message);
 //                      $location.path("/page-not-found");
+                    }
+                    if(status == 429){
+                        toaster.pop('error', "", data.detail + ". Please wait for " + data.availableIn);
                     }
 //                   $scope.errorMessage = data.message;
                  }); 
